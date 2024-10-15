a85bd3597ecd492d25cb0754c47ef479fc3e49c4
Nabil <lahlalia.nabil@hotmail.com>
2024-10-15 18:06:02 +0100

    adding eye hiding passord

--- emp-ui/src/app/modules/user/pages/change-password/change-password.component.ts
+++ emp-ui/src/app/modules/user/pages/change-password/change-password.component.ts
@@ -6,29 +6,29 @@ import { ChangePasswordRequest } from "../../../../models/ChangePasswordRequest"
 @Component({
   selector: 'app-change-password',
   templateUrl: './change-password.component.html',
-  styleUrls: ['./change-password.component.scss'] // Fix: styleUrls should be plural
+  styleUrls: ['./change-password.component.scss']
 })
 export class ChangePasswordComponent {
 
   errorMsg: Array<string> = [];
   request: ChangePasswordRequest = {currentPassword: '', newPassword: '', confirmationPassword: ''};
+  visible: boolean = true;
+  changeType: boolean = true;
 
   constructor(private userService: UserService, private router: Router) {}
+  viewpass(){
+    this.visible = !this.visible;
+    this.changeType= !this.changeType;
+  }
+
 
   changePassword() {
-    // Validation: Check if newPassword and confirmationPassword match
-    if (this.request.newPassword !== this.request.confirmationPassword) {
-      this.errorMsg = ['Passwords do not match'];
-      return;
-    }
-
     this.userService.changePassword(this.request).subscribe({
       next: (res) => {
         alert('Password changed successfully!');
         this.router.navigate(['/user']);
       },
       error: (err) => {
-        // Backend should send validation errors. If it's structured differently, adjust here.
         if (err.error && err.error.validationErrors) {
           this.errorMsg = err.error.validationErrors;
         } else {
