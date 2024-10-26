4334f878e652fb4721d58595d6fec1d33118ad09
Nabil <lahlalia.nabil@hotmail.com>
2024-10-26 14:27:48 +0100

    fiixng the method on the service rag

--- emp_api/src/main/java/net/lahlalia/emp_api/services/RagService.java
+++ emp_api/src/main/java/net/lahlalia/emp_api/services/RagService.java
@@ -1,7 +1,4 @@
-package net.lahlalia.emp_api.controllers;
-
-import lombok.RequiredArgsConstructor;
-import net.lahlalia.emp_api.services.RagService;
+package net.lahlalia.emp_api.services;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
@@ -10,34 +7,33 @@ import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
 
-@RestController
-@RequestMapping("/api/v1/chat")
-@RequiredArgsConstructor
-public class RagRestController {
+@Service
+public class RagService {
 
-    private final RagService ragService;
+    @Value("classpath:/prompts/prompt.st")
+    private Resource promptResource;
 
-    @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> ask(String question){
-       /*
+    private final ChatClient chatClient;
+    private final VectorStore vectorStore;
+
+    public RagService(ChatClient.Builder builder, VectorStore vectorStore){
+        this.chatClient = builder.build();
+        this.vectorStore = vectorStore;
+    }
+
+
+    public String ask(String question){
         PromptTemplate promptTemplate = new PromptTemplate(promptResource);
         List<Document> documents = vectorStore.similaritySearch(
                 SearchRequest.query(question).withTopK(4)
         );
         List<String> context = documents.stream().map(d-> d.getContent()).toList();
         Prompt prompt = promptTemplate.create(Map.of("context", context,"question",question));
-        */
-        String response =  ragService.ask(question);
-        return ResponseEntity.ok(response);
-
+        return chatClient.prompt(prompt).call().content();
     }
 }
